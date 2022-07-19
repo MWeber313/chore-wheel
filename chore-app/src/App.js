af8d8c803ccd4f04a763718aac5e4df300e8475d
MWeberLambdaweb19 <mack.webb37@gmail.com>
2022-07-19 15:06:27 -0700

    Skeleton ready for testing

--- chore-app/src/App.js
+++ chore-app/src/App.js
@@ -1,23 +1,61 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <table>
+        <tr>
+          <th><p><span id="jason">J</span>ason</p></th><br></br>
+          <th>-</th><br></br>
+          <th><p><span id="darian">D</span>arian</p></th><br></br>
+          <th><p><span id="amanda">A</span>manda</p></th><br></br>
+          <th><p><span id="miles">M</span>iles</p></th><br></br>
+          <th><p><span id="nat">N</span>atalie</p></th><br></br>
+        </tr>
+      </table>
+      <button>Click to Assign!</button>
+      <table>
+        <tr>
+          <th></th>
+          <th>Sunday </th><br></br>
+          <th>Monday </th><br></br>
+          <th>Tuesday </th><br></br>
+          <th>Wednesday </th><br></br>
+          <th>Thursday </th><br></br>
+          <th>Friday </th><br></br>
+          <th>Saturday </th><br></br>
+        </tr>
+        <tr>
+          <th>Dishes</th>
+          <td>N/A</td><br></br>
+          <td>N/A</td><br></br>
+          <td>N/A</td><br></br>
+          <td>N/A</td><br></br>
+          <td>N/A</td><br></br>
+          <td>N/A</td><br></br>
+          <td>N/A</td><br></br>
+        </tr>
+        <tr>
+          <th>Dishes</th>
+          <td>N/A</td><br></br>
+          <td>N/A</td><br></br>
+          <td>N/A</td><br></br>
+          <td>N/A</td><br></br>
+          <td>N/A</td><br></br>
+          <td>N/A</td><br></br>
+          <td>N/A</td><br></br> 
+        </tr>
+        <tr>
+          <th>Dishes</th>
+          <td>N/A</td><br></br>
+          <td>N/A</td><br></br>
+          <td>N/A</td><br></br>
+          <td>N/A</td><br></br>
+          <td>N/A</td><br></br>
+          <td>N/A</td><br></br>
+          <td>N/A</td><br></br> 
+        </tr>
+      </table>
     </div>
   );
 }
